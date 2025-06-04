56cbcc3adca2b68cc4ff97059b9c43300bce6fac
Rostom <88782107+Rostom12@users.noreply.github.com>
2025-06-04 16:03:57 +0200

    Delete pizzatech-main/pizzatech-main directory

--- pizzatech-main/pizzatech-main/backend/prisma/migrations/20250601124900_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Pizza" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "description" TEXT,
-    "price" REAL NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
--- pizzatech-main/pizzatech-main/backend/src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-export function errorHandler(err, req, res, next) {
-    console.error('[ERREUR]', err)
-
-    if (err.code === 'P2025') {
-        return res.status(404).json({ error: 'Élément non trouvé (Prisma)' })
-    }
-
-    res.status(500).json({ error: err.message || 'Erreur serveur' })
-}
